8e94f64fe3b0d232b6da51a5ab13202a36bb16aa
SabigBenmumin <totpophy14755@gmail.com>
2025-05-18 22:35:39 +0700

    prepare deploy

--- user_service/urls.py
+++ user_service/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.views import(
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/employeeinfo', EmployeeView.as_view(), name="employeeinfo")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
